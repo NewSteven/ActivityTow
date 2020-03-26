798138499d227cb53166bf78428c7e826f0e802b
NewSteven <36870147+NewSteven@users.noreply.github.com>
2020-03-26 18:52:56 -0500

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -3,18 +3,14 @@ package com.example.implicit_intents;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.provider.Telephony;
-import android.util.Log;
 import android.view.View;
 import android.content.Intent;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
 public class MainActivity extends AppCompatActivity {
 
-    private EditText EnviarTexto;
+    private EditText TextoVista1;
     private TextView TextoVista;
 
     @Override
@@ -22,17 +18,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EnviarTexto = (EditText)findViewById(R.id.EnviarTexto);
+        TextoVista1 = (EditText)findViewById(R.id.EnviarTexto);
 
-        TextoVista = (TextView)findViewById(R.id.TextoVista);
-        String dato1 = getIntent().getStringExtra("dato1");
-        TextoVista.setText((dato1));
+        TextoVista = (TextView)findViewById(R.id.textView1);
+        String vista2 = getIntent().getStringExtra("vista2");
+        TextoVista.setText((vista2));
     }
 
-    // metodo boto siguiente actividad
+    //metodo regresar actividad
     public void ActividadTow(View view){
         Intent actividadDos = new Intent(this, ActividadDos.class);
-        actividadDos.putExtra("dato1", EnviarTexto.getText().toString());
+        actividadDos.putExtra("vista1", TextoVista1.getText().toString());
         startActivity(actividadDos);
     }
 }
