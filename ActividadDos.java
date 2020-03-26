ba696f0a8652355f6303532efd1c913c5e4f6379
NewSteven <36870147+NewSteven@users.noreply.github.com>
2020-03-26 18:17:53 -0500

    Update ActividadDos.java

--- ActividadDos.java
+++ ActividadDos.java
@@ -6,15 +6,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class ActividadDos extends AppCompatActivity {
 
     private EditText EnviarTextoTwo;
+    private TextView TextoVista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actividad_dos);
+
+        EnviarTextoTwo = (EditText)findViewById(R.id.EnviarTexto);
+
+        TextoVista = (TextView)findViewById(R.id.TextoVista);
+        String dato1 = getIntent().getStringExtra("dato1");
+        TextoVista.setText((dato1));
     }
 
     //metodo regresar actividad
